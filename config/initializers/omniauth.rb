6fa462aeb9266f81603b7dabd31538f68a2b0ce8
Deepanhp <deepan.ppgit@gmail.com>
2018-03-21 17:26:19 +0530

    Initial commit

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, "639472520334-psq5fpsqap1mtb0oei9tod4gt7jl2qdl.apps.googleusercontent.com", "ljMRdnO7BCD9-8r6eL1ZJOgx"
+end
